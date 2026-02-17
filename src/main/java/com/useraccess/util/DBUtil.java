11a5f9180e5e000bbbf04f5ef1f41453a06c471f
kubanabaraka <byukusengekubanabarakapascal@gmail.com>
2026-02-17 17:57:40 +0200

    Update DB_PASSWORD in DBUtil class for database connection

--- src/main/java/com/useraccess/util/DBUtil.java
+++ src/main/java/com/useraccess/util/DBUtil.java
@@ -12,7 +12,7 @@ public class DBUtil {
 
     private static final String JDBC_URL = "jdbc:postgresql://127.0.0.1:5432/useraccess_db";
     private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "password";
+    private static final String DB_PASSWORD = "password123";
 
     static {
         try {
